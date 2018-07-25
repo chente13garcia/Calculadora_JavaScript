96273a199a2ac69cf81f562ae421c57da7bb3377
chentex <chente.13.garcia@gmail.com>
2018-07-24 19:08:20 -0500

    Programación de una calculadora en JavaScript

---
+++ js/app.js
@@ -0,0 +1,739 @@
+function init(){
+  var a;
+  var b;
+  var oper;
+}
+
+  upImageigual = new Image();
+  upImageigual.src = "image/igual.png";
+  downImageigual = new Image();
+  downImageigual.src = "image/igual_.png"
+  normalImageigual = new Image();
+  normalImageigual.src = "image/igual.png";
+
+  function changeImageigual()
+  {
+    document.images["igual"].src= upImageigual.src;
+    return true;
+  }
+  function handleMDownigual()
+  {
+    document.images["igual"].src = downImageigual.src;
+    return true;
+  }
+  function handleMUpigual()
+  {
+    changeImageigual();
+    return true;
+  }
+
+  upImagemas = new Image();
+  upImagemas.src = "image/mas.png";
+  downImagemas = new Image();
+  downImagemas.src = "image/mas_.png"
+  normalImagemas = new Image();
+  normalImagemas.src = "image/mas.png";
+
+  function changeImagemas()
+  {
+    document.images["mas"].src= upImagemas.src;
+    return true;
+  }
+  function handleMDownmas()
+  {
+    document.images["mas"].src = downImagemas.src;
+    return true;
+  }
+  function handleMUpmas()
+  {
+    changeImagemas();
+    return true;
+  }
+
+  upImagepun = new Image();
+  upImagepun.src = "image/punto.png";
+  downImagepun = new Image();
+  downImagepun.src = "image/punto_.png"
+  normalImagepun = new Image();
+  normalImagepun.src = "image/punto.png";
+
+  function changeImagepun()
+  {
+    document.images["punto"].src= upImagepun.src;
+    return true;
+  }
+  function handleMDownpun()
+  {
+    document.images["punto"].src = downImagepun.src;
+    return true;
+  }
+  function handleMUppun()
+  {
+    changeImagepun();
+    return true;
+  }
+
+  upImagecero = new Image();
+  upImagecero.src = "image/0.png";
+  downImagecero = new Image();
+  downImagecero.src = "image/0_.png"
+  normalImagecero = new Image();
+  normalImagecero.src = "image/0.png";
+
+  function changeImagecero()
+  {
+    document.images["cero"].src= upImagecero.src;
+    return true;
+  }
+  function handleMDowncero()
+  {
+    document.images["cero"].src = downImagecero.src;
+    return true;
+  }
+  function handleMUpcero()
+  {
+    changeImagecero();
+    return true;
+  }
+
+  upImageon = new Image();
+  upImageon.src = "image/ON.png";
+  downImageon = new Image();
+  downImageon.src = "image/ON_.png"
+  normalImageon = new Image();
+  normalImageon.src = "image/ON.png";
+
+  function changeImageon()
+  {
+    document.images["on"].src= upImageon.src;
+    return true;
+  }
+  function handleMDownon()
+  {
+    document.images["on"].src = downImageon.src;
+    return true;
+  }
+  function handleMUpon()
+  {
+    changeImageon();
+    return true;
+  }
+
+  upImagesign = new Image();
+  upImagesign.src = "image/sign.png";
+  downImagesign = new Image();
+  downImagesign.src = "image/sign_.png"
+  normalImagesign = new Image();
+  normalImagesign.src = "image/sign.png";
+
+  function changeImagesign()
+  {
+    document.images["sign"].src= upImagesign.src;
+    return true;
+  }
+  function handleMDownsign()
+  {
+    document.images["sign"].src = downImagesign.src;
+    return true;
+  }
+  function handleMUpsign()
+  {
+    changeImagesign();
+    return true;
+  }
+
+  upImageraiz = new Image();
+  upImageraiz.src = "image/raiz.png";
+  downImageraiz = new Image();
+  downImageraiz.src = "image/raiz_.png"
+  normalImageraiz = new Image();
+  normalImageraiz.src = "image/raiz.png";
+
+  function changeImageraiz()
+  {
+    document.images["raiz"].src= upImageraiz.src;
+    return true;
+  }
+  function handleMDownraiz()
+  {
+    document.images["raiz"].src = downImageraiz.src;
+    return true;
+  }
+  function handleMUpraiz()
+  {
+    changeImageraiz();
+    return true;
+  }
+
+  upImagediv = new Image();
+  upImagediv.src = "image/dividido.png";
+  downImagediv = new Image();
+  downImagediv.src = "image/dividido_.png"
+  normalImagediv = new Image();
+  normalImagediv.src = "image/dividido.png";
+
+  function changeImagediv()
+  {
+    document.images["dividido"].src= upImagediv.src;
+    return true;
+  }
+  function handleMDowndiv()
+  {
+    document.images["dividido"].src = downImagediv.src;
+    return true;
+  }
+  function handleMUpdiv()
+  {
+    changeImagediv();
+    return true;
+  }
+
+  upImagesiete = new Image();
+  upImagesiete.src = "image/7.png";
+  downImagesiete = new Image();
+  downImagesiete.src = "image/7_.png"
+  normalImagesiete = new Image();
+  normalImagesiete.src = "image/7.png";
+
+  function changeImagesiete()
+  {
+    document.images["siete"].src= upImagesiete.src;
+    return true;
+  }
+  function handleMDownsiete()
+  {
+    document.images["siete"].src = downImagesiete.src;
+    return true;
+  }
+  function handleMUpsiete()
+  {
+    changeImagesiete();
+    return true;
+  }
+
+  upImageocho = new Image();
+  upImageocho.src = "image/8.png";
+  downImageocho = new Image();
+  downImageocho.src = "image/8_.png"
+  normalImageocho = new Image();
+  normalImageocho.src = "image/8.png";
+
+  function changeImageocho()
+  {
+    document.images["ocho"].src= upImageocho.src;
+    return true;
+  }
+  function handleMDownocho()
+  {
+    document.images["ocho"].src = downImageocho.src;
+    return true;
+  }
+  function handleMUpocho()
+  {
+    changeImageocho();
+    return true;
+  }
+
+  upImagenueve = new Image();
+  upImagenueve.src = "image/9.png";
+  downImagenueve = new Image();
+  downImagenueve.src = "image/9_.png"
+  normalImagenueve = new Image();
+  normalImagenueve.src = "image/9.png";
+
+  function changeImagenueve()
+  {
+    document.images["nueve"].src= upImagenueve.src;
+    return true;
+  }
+  function handleMDownnueve()
+  {
+    document.images["nueve"].src = downImagenueve.src;
+    return true;
+  }
+  function handleMUpnueve()
+  {
+    changeImagenueve();
+    return true;
+  }
+
+  upImagepor = new Image();
+  upImagepor.src = "image/por.png";
+  downImagepor = new Image();
+  downImagepor.src = "image/por_.png"
+  normalImagepor = new Image();
+  normalImagepor.src = "image/por.png";
+
+  function changeImagepor()
+  {
+    document.images["por"].src= upImagepor.src;
+    return true;
+  }
+  function handleMDownpor()
+  {
+    document.images["por"].src = downImagepor.src;
+    return true;
+  }
+  function handleMUppor()
+  {
+    changeImagepor();
+    return true;
+  }
+
+  upImagecuatro = new Image();
+  upImagecuatro.src = "image/4.png";
+  downImagecuatro = new Image();
+  downImagecuatro.src = "image/4_.png"
+  normalImagecuatro = new Image();
+  normalImagecuatro.src = "image/4.png";
+
+  function changeImagecuatro()
+  {
+    document.images["cuatro"].src= upImagecuatro.src;
+    return true;
+  }
+  function handleMDowncuatro()
+  {
+    document.images["cuatro"].src = downImagecuatro.src;
+    return true;
+  }
+  function handleMUpcuatro()
+  {
+    changeImagecuatro();
+    return true;
+  }
+
+  upImagecinco = new Image();
+  upImagecinco.src = "image/5.png";
+  downImagecinco = new Image();
+  downImagecinco.src = "image/5_.png"
+  normalImagecinco = new Image();
+  normalImagecinco.src = "image/5.png";
+
+  function changeImagecinco()
+  {
+    document.images["cinco"].src= upImagecinco.src;
+    return true;
+  }
+  function handleMDowncinco()
+  {
+    document.images["cinco"].src = downImagecinco.src;
+    return true;
+  }
+  function handleMUpcinco()
+  {
+    changeImagecinco();
+    return true;
+  }
+
+  upImageseis = new Image();
+  upImageseis.src = "image/6.png";
+  downImageseis = new Image();
+  downImageseis.src = "image/6_.png"
+  normalImageseis= new Image();
+  normalImageseis.src = "image/6.png";
+
+  function changeImageseis()
+  {
+    document.images["seis"].src= upImageseis.src;
+    return true;
+  }
+  function handleMDownseis()
+  {
+    document.images["seis"].src = downImageseis.src;
+    return true;
+  }
+  function handleMUpseis()
+  {
+    changeImageseis();
+    return true;
+  }
+
+  upImagemenos = new Image();
+  upImagemenos.src = "image/menos.png";
+  downImagemenos = new Image();
+  downImagemenos.src = "image/menos_.png"
+  normalImagemenos = new Image();
+  normalImagemenos.src = "image/menos.png";
+
+  function changeImagemenos()
+  {
+    document.images["menos"].src= upImagemenos.src;
+    return true;
+  }
+  function handleMDownmenos()
+  {
+    document.images["menos"].src = downImagemenos.src;
+    return true;
+  }
+  function handleMUpmenos()
+  {
+    changeImagemenos();
+    return true;
+  }
+
+  upImageuno = new Image();
+  upImageuno.src = "image/1.png";
+  downImageuno = new Image();
+  downImageuno.src = "image/1_.png"
+  normalImageuno = new Image();
+  normalImageuno.src = "image/1.png";
+
+  function changeImageuno()
+  {
+    document.images["uno"].src= upImageuno.src;
+    return true;
+  }
+  function handleMDownuno()
+  {
+    document.images["uno"].src = downImageuno.src;
+    return true;
+  }
+  function handleMUpuno()
+  {
+    changeImageuno();
+    return true;
+  }
+
+  upImagedos = new Image();
+  upImagedos.src = "image/2.png";
+  downImagedos = new Image();
+  downImagedos.src = "image/2_.png"
+  normalImagedos = new Image();
+  normalImagedos.src = "image/2.png";
+
+  function changeImagedos()
+  {
+    document.images["dos"].src= upImagedos.src;
+    return true;
+  }
+  function handleMDowndos()
+  {
+    document.images["dos"].src = downImagedos.src;
+    return true;
+  }
+  function handleMUpdos()
+  {
+    changeImagedos();
+    return true;
+  }
+
+  upImagetres = new Image();
+  upImagetres.src = "image/3.png";
+  downImagetres = new Image();
+  downImagetres.src = "image/3_.png"
+  normalImagetres = new Image();
+  normalImagetres.src = "image/3.png";
+
+  function changeImagetres()
+  {
+    document.images["tres"].src= upImagetres.src;
+    return true;
+  }
+  function handleMDowntres()
+  {
+    document.images["tres"].src = downImagetres.src;
+    return true;
+  }
+  function handleMUptres()
+  {
+    changeImagetres();
+    return true;
+  }
+
+  var display = document.getElementById('display');
+  var on = document.getElementById('on');
+  var suma = document.getElementById('mas');
+  var resta = document.getElementById('menos');
+  var multiplicacion = document.getElementById('por');
+  var division = document.getElementById('dividido');
+  var igual = document.getElementById('igual');
+  var uno = document.getElementById('uno');
+  var dos = document.getElementById('dos');
+  var tres = document.getElementById('tres');
+  var cuatro = document.getElementById('cuatro');
+  var cinco = document.getElementById('cinco');
+  var seis = document.getElementById('seis');
+  var siete = document.getElementById('siete');
+  var ocho = document.getElementById('ocho');
+  var nueve = document.getElementById('nueve');
+  var cero = document.getElementById('cero');
+
+  uno.onclick = function(e){
+    if (display.textContent == "0") {
+      limpiar();
+      display.textContent = display.textContent  + "1";
+    }
+    else
+    {
+      var caddis = display.textContent;
+      var x = caddis.length;
+      if (x < 8)
+      {
+        display.textContent = display.textContent  + "1";
+      }
+      caddis = " ";
+      x = x + 0;
+    }
+}
+  dos.onclick = function(e){
+    if (display.textContent == "0") {
+      limpiar();
+      display.textContent = display.textContent  + "2";
+    }
+    else
+    {
+      var caddis = display.textContent;
+      var x = caddis.length;
+      if (x < 8)
+      {
+        display.textContent = display.textContent  + "2";
+      }
+      caddis = " ";
+      x = x + 0;
+    }
+}
+  tres.onclick = function(e){
+    if (display.textContent == "0") {
+      limpiar();
+      display.textContent = display.textContent  + "3";
+    }
+    else
+    {
+      var caddis = display.textContent;
+      var x = caddis.length;
+      if (x < 8)
+      {
+        display.textContent = display.textContent  + "3";
+      }
+      caddis = " ";
+      x = x + 0;
+    }
+}
+  cuatro.onclick = function(e){
+    if (display.textContent == "0") {
+      limpiar();
+      display.textContent = display.textContent  + "4";
+    }
+    else
+    {
+      var caddis = display.textContent;
+      var x = caddis.length;
+      if (x < 8)
+      {
+        display.textContent = display.textContent  + "4";
+      }
+      caddis = " ";
+      x = x + 0;
+    }
+}
+  cinco.onclick = function(e){
+    if (display.textContent == "0") {
+      limpiar();
+      display.textContent = display.textContent  + "5";
+    }
+    else
+    {
+      var caddis = display.textContent;
+      var x = caddis.length;
+      if (x < 8)
+      {
+        display.textContent = display.textContent  + "5";
+      }
+      caddis = " ";
+      x = x + 0;
+    }
+}
+  seis.onclick = function(e){
+    if (display.textContent == "0") {
+      limpiar();
+      display.textContent = display.textContent  + "6";
+    }
+    else
+    {
+      var caddis = display.textContent;
+      var x = caddis.length;
+      if (x < 8)
+      {
+        display.textContent = display.textContent  + "6";
+      }
+      caddis = " ";
+      x = x + 0;
+    }
+}
+  siete.onclick = function(e){
+    if (display.textContent == "0") {
+      limpiar();
+      display.textContent = display.textContent  + "7";
+    }
+    else
+    {
+      var caddis = display.textContent;
+      var x = caddis.length;
+      if (x < 8)
+      {
+        display.textContent = display.textContent  + "7";
+      }
+      caddis = " ";
+      x = x + 0;
+    }
+}
+  ocho.onclick = function(e){
+    if (display.textContent == "0") {
+      limpiar();
+      display.textContent = display.textContent  + "8";
+    }
+    else
+    {
+      var caddis = display.textContent;
+      var x = caddis.length;
+      if (x < 8)
+      {
+        display.textContent = display.textContent  + "8";
+      }
+      caddis = " ";
+      x = x + 0;
+    }
+}
+  nueve.onclick = function(e){
+      if (display.textContent == "0") {
+        limpiar();
+        display.textContent = display.textContent  + "9";
+      }
+      else
+      {
+        var caddis = display.textContent;
+        var x = caddis.length;
+        if (x < 8)
+        {
+          display.textContent = display.textContent  + "9";
+        }
+        caddis = " ";
+        x = x + 0;
+      }
+  }
+  cero.onclick = function(e){
+    if ( display.textContent == 0)
+    {
+      display.textContent = display.textContent  + "";
+    }
+    else
+    {
+      if ( display.textContent == '0.')
+      {
+        display.textContent = display.textContent  + "0.00";
+      }
+      else
+      {
+        var caddis = display.textContent;
+        var x = caddis.length;
+        if (x < 8)
+        {
+          display.textContent = display.textContent  + "0";
+        }
+        caddis = " ";
+        x = x + 0;
+      }
+    }
+  }
+  punto.onclick = function(e){
+    if ( display.textContent == 0)
+    {
+      limpiar();
+      display.textContent = display.textContent  + "0.";
+    }
+    else
+    {
+      if ( display.textContent.indexOf(".") == -1)
+      {
+        display.textContent = display.textContent + ".";
+      }
+    }
+    document.Pantalla.Display.value = display.textContent;
+  }
+  on.onclick = function(e){
+      resetear();
+  }
+  suma.onclick = function(e){
+      a = display.textContent;
+      oper = "+";
+      limpiar();
+  }
+  resta.onclick = function(e){
+      a = display.textContent;
+      oper = "-";
+      limpiar();
+  }
+  multiplicacion.onclick = function(e){
+      a = display.textContent;
+      oper = "*";
+      limpiar();
+  }
+  division.onclick = function(e){
+      a = display.textContent;
+      oper = "/";
+      limpiar();
+  }
+  igual.onclick = function(e){
+      b = display.textContent;
+      resolver();
+  }
+
+  sign.onclick = function(e) {
+      var tempo = Number(display.textContent);
+      if (tempo > 0) {
+        tempo = tempo * -1;
+        display.textContent = String(tempo);
+      }
+      else
+      {
+        if (tempo < 0) {
+          tempo = tempo * -1;
+          display.textContent = String(tempo);
+        }
+      }
+   }
+
+function limpiar(){
+  display.textContent = "";
+}
+function resetear(){
+  display.textContent = "0";
+  a = 0;
+  b = 0;
+  oper = "";
+}
+
+function resolver(){
+  var res = 0;
+  switch(oper){
+    case "+":
+      res = parseFloat(a) + parseFloat(b);
+      break;
+    case "-":
+        res = parseFloat(a) - parseFloat(b);
+        break;
+    case "*":
+      res = parseFloat(a) * parseFloat(b);
+      if ((res > 999999999)) {
+        var cadena = String(res);
+        var m = cadena.length;
+        var inicio = 0;
+        var fin    = 3;
+        var restemp = cadena.substring(inicio, fin);
+        res = (restemp/100) + "E+" + (m-1) ;
+      }
+
+      break;
+    case "/":
+      res = parseFloat(a) / parseFloat(b);
+      var cadena = String(res);
+      var inicio = 0;
+      var fin    = 10;
+      var restemp = cadena.substring(inicio, fin);
+      res = Number(restemp);
+      break;
+  }
+  resetear();
+  display.textContent = res;
+}
